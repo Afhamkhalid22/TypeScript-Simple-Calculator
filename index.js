ddad94b46d7b2dd682e4d9362fc2d14efb6a9f5e
Afhamkhalid22 <Muhammadafhamkhalid@gmail.com>
2024-04-27 11:51:10 -0700

    Adding Simple Calculator Project

---
+++ index.js
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.red.bold("\n \t\t Welcome to 'code with afham' - calculator\n"));
+// Asking Questions from Users through Inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "Division"],
+    },
+]);
+// Condtional statements If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
